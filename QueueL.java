74a123d21cf623a084704b02aeeae5413f4fcfe1
zezomido78 <48326712+zezomido78@users.noreply.github.com>
2019-05-04 19:55:39 +0200

    Add files via upload

---
+++ QueueL.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class QueueL implements IQueue , ILinkedBased {
+	class DNode {
+        Object element;
+        DNode Next;
+        DNode Prev;
+
+        DNode(Object Value, DNode next, DNode prev) {
+            element = Value;
+            Next = next;
+            Prev = prev;
+        }
+    }
+	DNode head;
+    DNode tail;
+    int size;
+
+    QueueL() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    boolean isEmpty() {
+    	return (size==0);
+    }
+    @Override
+    public void enqueue(Object element){
+    	DNode node= new DNode(element,null,null)
+        head.Prev=node;
+        node.Next=head;
+        head=node;
+        size++;
+        if(tail==null)tail=node;
+    }
+    @Override
+    public Object dequeue() {
+    	Object e = new Object();
+    	if(isEmpty()) {
+    		throw new IllegalArgumentException("Queue is empty");
+    	}
+    	e=tail.element;
+    	tail=tail.Prev;
+    	tail.Next=null;
+    	size--;
+    	return e;
+    	
+    }
+    @Override
+    public int size() {
+    	return size;
+    }
+    public Object front() {
+    	if(isEmpty()) {
+    		throw new IllegalArgumentException("Queue is Empty");
+    		
+    	}
+    	return tail.element;
+    }
+	
+
+}
